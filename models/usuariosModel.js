7b6d8581b3d15a5829493134e7f93b2aaec27e31
Victor Hugo Gonçalves <vh.goncalves2007@gmail.com>
2025-05-23 19:20:14 +0000

    Modelo de dados criado

---
+++ models/usuariosModel.js
@@ -0,0 +1,22 @@
+const { type } = require('express/lib/response');
+const mongoose = require('mongoose');
+
+const usuarioSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+});
+
+const Usuario = mongoose.model('Usuario', usuarioSchema, 'usuarios');
+
+module.exports = Usuario;
